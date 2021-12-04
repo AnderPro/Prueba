15e444ba615eafb0b0ff50c0b552b795673b3187
andersonrev <fabiander_21@hotmail.es>
2021-12-04 13:16:06 -0500

    update: estilos inline

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,15 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+const estilo2 = {
+  boxShadow: '0 5px 3px rgba(0, 0, 0, 0.5)'
+}
+
+const estilo1 = ({bg = '#222'})=>({
+  backgroundColor: bg,
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15px'
+});
+
 const Li = ({children,estado}) => {
   return (
-    <li className="clase-li">{children} = {estado}</li>
+    <li style={{...estilo2, ...estilo1({bg: '#333'})}} 
+        className="clase-li">
+        {children} = {estado}
+    </li>
   )
 }
 const App = () => {
   const valor = 'triste';
   return (
-    <ul className="clase-css">
+    <ul  style={estilo1({bg:'#750'})} className="clase-css">
       <Li estado="feliz">valor de li</Li>
     </ul>
   );
